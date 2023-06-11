5d4b6c38c7b8ebff10b5382c90fa7abab9dee9f8
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-11 20:19:37 +0100

    sqdqs

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,200 +1,180 @@
 const orders = require("../models/orders");
 const Dish = require("../models/dish");
-const FCM = require('fcm-node');
+const FCM = require("fcm-node");
 const server_key = require("../FireBaseConfig");
 const fcmServerKey = new FCM(server_key.SERVER_KEY);
 const stream = require("stream");
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 //create a new order
-exports.createOrder = async(req, res, next) => {
+exports.createOrder = async (req, res, next) => {
+  const { date_order } = req.body;
 
-    const { date_order } = req.body;
-
-    try {
-
-        if (!date_order) {
-            req.body.date_order = new Date().toISOString().replace('T', ' ').substr(0, 10);
-        }
-        const order = await orders.create(req.body);
-        // PushNotifications("ExponentPushToken[idAMq6K8wibcgqLWNp_jCt]");
-        res.status(201).json({
-            success: true,
-            order
-        })
-
-    } catch (error) {
-        console.log(error);
-        next(error);
-
+  try {
+    if (!date_order) {
+      req.body.date_order = new Date()
+        .toISOString()
+        .replace("T", " ")
+        .substr(0, 10);
     }
+    const order = await orders.create(req.body);
+    // PushNotifications("ExponentPushToken[idAMq6K8wibcgqLWNp_jCt]");
+    res.status(201).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
-
 //getOrders
-exports.getOrders = async(req, res, next) => {
+exports.getOrders = async (req, res, next) => {
+  const { date_order, status } = req.body;
+  try {
+    let order;
 
-    const { date_order, status } = req.body;
-    try {
-        let order;
-
-        if (!status && !date_order) {
-            order = await orders.find();
-        } else if (!status && date_order) {
-            order = await orders.find({ date_order: date_order });
-        } else {
-            order = await orders.find({ date_order: date_order, status: status });
-        }
-
-        res.status(201).json({
-            success: true,
-            order
-        })
-
-    } catch (error) {
-        console.log(error);
-        next(error);
-
+    if (!status && !date_order) {
+      order = await orders.find();
+    } else if (!status && date_order) {
+      order = await orders.find({ date_order: date_order });
+    } else {
+      order = await orders.find({ date_order: date_order, status: status });
     }
 
+    res.status(201).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 //getOrders Status
-exports.getOrdersStatus = async(req, res, next) => {
+exports.getOrdersStatus = async (req, res, next) => {
+  const { status } = req.body;
+  try {
+    const date = new Date().toISOString().replace("T", " ").substr(0, 10);
+    const order = await orders.find({ date_order: date, status: status });
+    console.log("order =", order.length);
 
-    const { status } = req.body;
-    try {
-
-        const date = new Date().toISOString().replace('T', ' ').substr(0, 10);
-        const order = await orders.find({ date_order: date, status: status });
-        console.log("order =", order.length);
-
-        res.status(201).json({
-            success: true,
-            order: order,
-            orderlength: order.length
-        })
-
-    } catch (error) {
-        console.log(error);
-        next(error);
-
-    }
-
+    res.status(201).json({
+      success: true,
+      order: order,
+      orderlength: order.length,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 // Update Order
-exports.updateOrder = async(req, res, next) => {
+exports.updateOrder = async (req, res, next) => {
+  try {
+    const { id_order } = req.body;
 
-    try {
+    const orderStatus = await orders.findById(id_order);
 
-        const { id_order } = req.body;
+    if (orderStatus.status == 1) {
+      const order = await orders.findByIdAndUpdate({ _id: id_order }, req.body);
 
-        const orderStatus = await orders.findById(id_order);
-
-        if (orderStatus.status == 1) {
-            const order = await orders.findByIdAndUpdate({ _id: id_order }, req.body);
-
-            const updatedOrder = await orders.find({ _id: id_order });
-            monitorListingsUsingStreamApi(new MongoClient(process.env.DATABASE))
-            res.status(201).json({
-                success: true,
-                updatedOrder
-            });
-        } else {
-            res.status(400).json({
-                success: false,
-                message: "Order is already done!"
-            });
-        }
-
-    } catch (error) {
-        console.log(error);
-        next(error);
+      const updatedOrder = await orders.find({ _id: id_order });
+      monitorListingsUsingStreamApi(new MongoClient(process.env.DATABASE));
+      res.status(201).json({
+        success: true,
+        updatedOrder,
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Order is already done!",
+      });
     }
-
-
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
-
 // Delete Order
-exports.deleteOrder = async(req, res, next) => {
+exports.deleteOrder = async (req, res, next) => {
+  try {
+    const { id_order } = req.body;
 
-    try {
-        const { id_order } = req.body;
+    const orderStatus = await orders.findById(id_order);
+    console.log("orderStatus =", orderStatus);
 
-        const orderStatus = await orders.findById(id_order);
-        console.log("orderStatus =", orderStatus);
+    if (orderStatus.status == 1) {
+      const order = await orders.findByIdAndRemove({ _id: id_order }, req.body);
 
-        if (orderStatus.status == 1) {
-            const order = await orders.findByIdAndRemove({ _id: id_order }, req.body);
-
-            res.status(201).json({
-                success: true,
-                message: "Order deleted successfully"
-            });
-        } else {
-            res.status(400).json({
-                success: false,
-                message: "Order is already done!"
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        next(error);
+      res.status(201).json({
+        success: true,
+        message: "Order deleted successfully",
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Order is already done!",
+      });
     }
-
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 // Confirm order
-exports.confirmOrder = async(req, res, next) => {
-
-    try {
-        const { id_order } = req.body;
-        const order = await orders.findByIdAndUpdate({ _id: id_order }, req.body);
-        res.status(201).json({
-            success: true,
-            message: "Order confirmed successfully"
-        });
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
+exports.confirmOrder = async (req, res, next) => {
+  try {
+    const { id_order } = req.body;
+    const order = await orders.findByIdAndUpdate({ _id: id_order }, req.body);
+    res.status(201).json({
+      success: true,
+      message: "Order confirmed successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
-
-
-
 // Push Notification
-const monitorListingsUsingStreamApi = async(client, timeInMs = 60000, pipeLine = []) => {
-    const collection = client.db("ECJA_V2").collection("orders");
+const monitorListingsUsingStreamApi = async (
+  client,
+  timeInMs = 60000,
+  pipeLine = []
+) => {
+  const collection = client.db("ECJA_V2").collection("orders");
 
-    const changeStream = collection.watch(pipeLine);
-    // console.log("changeStream =", changeStream);
-    changeStream.stream().pipe(
-        new stream.Writable({
-            objectMode: true,
-            write: (doc, _, cb) => {
-                console.log('doc = ', doc);
-                cb();
-            }
-        })
-    );
+  const changeStream = collection.watch(pipeLine);
+  // console.log("changeStream =", changeStream);
+  changeStream.stream().pipe(
+    new stream.Writable({
+      objectMode: true,
+      write: (doc, _, cb) => {
+        console.log("doc = ", doc);
+        cb();
+      },
+    })
+  );
 
-    await closeChangeStream(timeInMs, changeStream);
-}
+  await closeChangeStream(timeInMs, changeStream);
+};
 
 const closeChangeStream = (timeInMs = 60000, changeStream) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("Closing the change stream");
-                changeStream.close();
-                resolve();
-            }, timeInMs);
-        });
-    }
-    // const PushNotifications = async(device_token) => {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Closing the change stream");
+      changeStream.close();
+      resolve();
+    }, timeInMs);
+  });
+};
+// const PushNotifications = async(device_token) => {
 
 //     const message = {
 //         to: device_token,
