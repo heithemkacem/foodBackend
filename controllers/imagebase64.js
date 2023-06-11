0e0d72f34f85aad2cd96044800cd083682f49911
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-11 21:53:52 +0100

    qs,kfqlkafa

--- controllers/imagebase64.js
+++ controllers/imagebase64.js
@@ -1,17 +1,12 @@
-const imagebase64 = require('../models/imagebase64');
-const nodeBase64 = require('nodejs-base64-converter');
+const imagebase64 = require("../models/imagebase64");
 
-
-exports.uploadImage = async(req, res, next) => {
-
-    const image = req.body.image;
-    try {
-        const base64 = await imagebase64.create(req.body);
-        const decodebase64 = nodeBase64.decode(image);
-
-        res.status(201).json({ success: true, link: decodebase64 });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({ success: false, message: error.message });
-    }
-}
+exports.uploadImage = async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const base64 = await imagebase64.create(req.body);
+    res.status(201).json({ success: true, base64 });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
--- models/imagebase64.js
+++ models/imagebase64.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-
-const images = new mongoose.Schema({
-
-    "id_dish": {
-        type: String,
-        required: true
+const images = new mongoose.Schema(
+  {
+    id_dish: {
+      type: String,
+      required: true,
     },
-    "image": {
-        type: String,
-        required: true
+    image: {
+      type: String,
+      required: true,
     },
-
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("qr_images", images);
