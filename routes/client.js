cee82e5c991a57b6a484511e97f785740a903202
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-13 21:44:07 +0100

    add ; print order function

--- routes/client.js
+++ routes/client.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Client = require('../controllers/client');
+const Client = require("../controllers/client");
 
-router.post('/signup', Client.signup);
-router.post('/login', Client.login);
-router.post('/logout', Client.logout);
-router.post('/updatePassword', Client.updatePassword);
-router.post('/sendMail', Client.sendMail);
-router.post('/resetPassword', Client.resetPassword);
+router.post("/signup", Client.signup);
+router.post("/login", Client.login);
+router.post("/logout", Client.logout);
+router.post("/updatePassword", Client.updatePassword);
+router.post("/sendMail", Client.sendMail);
+router.post("/resetPassword", Client.resetPassword);
+router.post("/printOrder", Client.printOrder);
 
 module.exports = router;
